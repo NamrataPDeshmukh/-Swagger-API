5ed184dec91661e0ab2775c627a1a368f9683ee5
NamrataPDeshmukh <161303191+NamrataPDeshmukh@users.noreply.github.com>
2024-03-16 15:50:23 +0530

    Add files via upload

---
+++ Models/Student.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace StudentAPIdemo.Models
+{
+    public class Student
+    {
+        [Key]
+        public string RollNo { get; set; }
+        public string Name { get; set; }    
+    }
+}
